c685bbaf9bcdd7e66aa4d43905b72661ad2e9477
Serafin Piña <arespin23@gmail.com>
2023-09-22 09:22:24 +0200

    second commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { addRxPlugin } from 'rxdb';
+import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
+addRxPlugin(RxDBDevModePlugin);
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [provideRouter(routes)]
 };
